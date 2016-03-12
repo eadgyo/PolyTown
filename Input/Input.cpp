1651a5673a51963ce96dd8df2b029551f8086790
Ronan <ronanjamet.h@gmail.com>
2016-03-12 15:03:23 +0100

    Réajout Input

---
+++ Input/Input.cpp
@@ -0,0 +1,36 @@
+#include "Input.h"
+
+#include <iostream>
+#include <SDL2/SDL.h>
+
+using namespace std;
+
+Input Input::instance = Input();
+
+Input::Input() {}
+
+Input Input::getInstance()
+{
+    return instance;
+}
+
+void Input::swapSDL()
+{
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) {
+        /* We are only worried about SDL_KEYDOWN and SDL_KEYUP events */
+        switch (event.type) {
+            case SDL_KEYDOWN:
+                cout << "Key press detected" << endl;
+                break;
+
+            case SDL_KEYUP:
+                cout << "Key release detected" << endl;
+                break;
+
+            default:
+                break;
+        }
+    }
+}
---
+++ Input/mainInput.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <time.h>
+#include <SDL2/SDL.h>
+
+#include "Input.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    Input input = Input::getInstance();
+
+    SDL_Window *window;
+
+    window = SDL_CreateWindow(
+        "An SDL2 window",                  // window title
+        SDL_WINDOWPOS_UNDEFINED,           // initial x position
+        SDL_WINDOWPOS_UNDEFINED,           // initial y position
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+        );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+    int i(0);
+
+    while (i < 5) {
+        input.update();
+        SDL_Delay(3000);
+
+        i++;
+    }
+
+                      // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean up
+    SDL_Quit();
+
+    return 0;
+}

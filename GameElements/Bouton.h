331e83cf6321fd3f9d3974146944b08c616c1cfe
RonanH <littleybird2@gmail.com>
2016-03-21 14:50:33 +0100

    Correction BoutonImage

--- GameElements/Bouton.h
+++ GameElements/Bouton.h
@@ -9,16 +9,17 @@ public:
 	Bouton(int x, int y, int width, int height);
 	Bouton(std::string text, int x, int y, int width, int height);
 	Bouton(std::string text, unsigned int size, int x, int y, int width, int height);
-	~Bouton();
+	virtual ~Bouton();
 
 	void setColor(float r, float g, float b, float a);
 
 	void update(float dt);
 	void render(Graphics* g);
 	void render(Graphics* g, const Vector3D& translation);
+
 	bool isColliding(const Vector3D& pos);
 
-private:
+protected:
 	float r, g, b, a;
 	sRectangle rectangle;
 	std::string text;
--- GameElements/BoutonImage.h
+++ GameElements/BoutonImage.h
@@ -2,11 +2,13 @@
 
 #include "Bouton.h"
 
-class BoutonImage : public virtual Bouton
+class BoutonImage : public Bouton
 {
 public:
     BoutonImage(Image* image, int x, int y, int width, int height);
+	void render(Graphics* graphics);
+	void render(Graphics* graphics, const Vector3D& translation);
 
-private:
+protected:
     Image* m_image;
 };
